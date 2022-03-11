a5f160ecd0625e8c555187b0e375264fe0c963ad
Vicky AS <65887817+vick1208@users.noreply.github.com>
2022-03-11 20:59:05 +0700

    login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,10 +6,10 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
 
     public function index(){
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[HomeController::class,"index"])->name('home');
 
+Route::view('/login', 'auth.login')->name('login');
+
 
